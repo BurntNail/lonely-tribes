19b0436d2d8ec829d94309ffbbc8dbd28f1ddb47
Jack Maguire <jackmaguire1234@gmail.com>
2021-08-09 18:46:03 +0100

    Add script to automate new release

--- new_release.sh
+++ new_release.sh
@@ -6,6 +6,6 @@ cargo build --release
 
 cp -r assets Releases/new-release/
 cp -r config Releases/new-release/
-cp -r target/release/making-friends Releases/new-release
+cp -r target/release/lonely-tribes Releases/new-release
 
 rm -rf Releases/new-release/assets/data/*
