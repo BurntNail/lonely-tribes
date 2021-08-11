bc3efe4dff3ce97e9bac65d20a653d8bb3b95a54
Jack Maguire <jackmaguire1234@gmail.com>
2021-08-11 21:06:49 +0100

    Farm out hiding and showing to different methods.

--- src/states/paused_state.rs
+++ src/states/paused_state.rs
@@ -1,6 +1,9 @@
 use crate::components::data_holder::EntityHolder;
 use amethyst::{
-    core::{ecs::WorldExt, Hidden},
+    core::{
+        ecs::{Entity, World, WorldExt},
+        Hidden,
+    },
     input::{InputEvent, VirtualKeyCode},
     GameData, SimpleState, SimpleTrans, StateData, StateEvent,
 };
@@ -21,16 +24,11 @@ pub struct PausedState;
 impl SimpleState for PausedState {
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
         let world = data.world;
-        world.insert(MovementDisabler { enabled: true });
-        let entities = world.read_resource::<EntityHolder>();
-        let mut hiddens = world.write_storage::<Hidden>();
 
-        entities.get_all_entities().into_iter().for_each(|ent| {
-            hiddens.insert(ent, Hidden).unwrap_or_else(|err| {
-                log::warn!("Unable to hide entity: {}", err);
-                None
-            });
-        });
+        world.insert(MovementDisabler { enabled: true });
+
+        let entities = world.read_resource::<EntityHolder>();
+        hide_entities(world, entities.get_all_entities());
     }
 
     fn handle_event(
@@ -44,21 +42,39 @@ impl SimpleState for PausedState {
                 t = SimpleTrans::Pop;
 
                 let world = data.world;
-                let mut disabler = world.write_resource::<MovementDisabler>();
-                disabler.enabled = false;
+                world.insert(MovementDisabler::default());
 
                 let entities = world.read_resource::<EntityHolder>();
-                let mut hiddens = world.write_storage::<Hidden>();
-
-                entities.get_all_entities().into_iter().for_each(|ent| {
-                    hiddens.remove(ent).unwrap_or_else(|| {
-                        log::warn!("Unable to show entity.");
-                        Hidden
-                    });
-                });
+                show_entities(world, entities.get_all_entities())
             }
         }
 
         t
     }
+}
+
+///Function to hide a given list of entities, by adding Hidden components to all of them
+pub fn hide_entities(world: &mut World, entities: Vec<Entity>) {
+    let mut hiddens = world.write_storage::<Hidden>();
+
+    entities.into_iter().for_each(|ent| {
+        hiddens.insert(ent, Hidden).unwrap_or_else(|err| {
+            log::warn!("Unable to hide entity: {}", err);
+            None
+        });
+    });
+}
+
+///Function to show a given list of entities, by removing Hidden components from all of them
+///
+///If they don't have a Hidden component, then it does a log::warn
+pub fn show_entities(world: &mut World, entities: Vec<Entity>) {
+    let mut hiddens = world.write_storage::<Hidden>();
+
+    entities.into_iter().for_each(|ent| {
+        hiddens.remove(ent).unwrap_or_else(|| {
+            log::warn!("Unable to show entity: {:?}", ent);
+            Hidden
+        });
+    });
 }
