17dc0fe73d6e25241fd0cc09265df069f66345fe
Jack Maguire <jackmaguire1234@gmail.com>
2021-09-14 13:07:00 +0100

    rework win system to allow for sfx on merge

--- src/systems/player_overlap_checker.rs
+++ src/systems/player_overlap_checker.rs
@@ -1,24 +1,34 @@
-use amethyst::core::ecs::{System, Entity, WriteStorage, ReadStorage, Join, Entities, Write};
-use crate::components::player::Player;
-use crate::components::tile_transform::TileTransform;
+use crate::components::{
+    player::Player,
+    tile_transform::TileTransform,
+    win_state::{GameState, GameStateEnum},
+};
+use amethyst::core::ecs::{Entities, Entity, Join, ReadStorage, System, Write, WriteStorage};
 use std::collections::HashMap;
-use crate::components::win_state::{GameState, GameStateEnum};
 
 pub struct PlayerOverlapChecker;
 
-impl <'s> System<'s> for PlayerOverlapChecker {
-    type SystemData = (WriteStorage<'s, Player>, ReadStorage<'s, TileTransform>, Entities<'s>, Write<'s, DeleteList>, Write<'s, GameState>);
+impl<'s> System<'s> for PlayerOverlapChecker {
+    type SystemData = (
+        WriteStorage<'s, Player>,
+        ReadStorage<'s, TileTransform>,
+        Entities<'s>,
+        Write<'s, DeleteList>,
+        Write<'s, GameState>,
+    );
 
     fn run(&mut self, (mut players, tiles, entities, mut delete_list, mut gs): Self::SystemData) {
         let mut map: HashMap<TileTransform, Player> = HashMap::new();
         let mut id_list = Vec::new();
         for (e, p, t) in (&entities, &mut players, &tiles).join() {
-            let to_insert = if let Some(mut current) = map.remove(&t) {
+            let to_insert = if let Some(mut current) = map.remove(t) {
                 if current.id == p.id {
                     current.no_players += 1;
                     delete_list.0.push(e);
                 } else {
-                    gs.ws = GameStateEnum::End {lost_position: Some(*t)};
+                    gs.ws = GameStateEnum::End {
+                        lost_position: Some(*t),
+                    };
                 }
                 current
             } else {
@@ -33,10 +43,12 @@ impl <'s> System<'s> for PlayerOverlapChecker {
         }
 
         if map.len() == id_list.len() {
-            gs.ws = GameStateEnum::End {lost_position: None};
+            gs.ws = GameStateEnum::End {
+                lost_position: None,
+            };
         }
     }
 }
 
 #[derive(Default)]
-pub struct DeleteList (pub Vec<Entity>);
+pub struct DeleteList(pub Vec<Entity>);
