351d879f85e9cfeb008fe3067e5eb72f45f7a38b
Jack Maguire <jackmaguire1234@gmail.com>
2021-09-18 10:32:32 +0100

    lt_fog_of_war

---
+++ lt_fog_of_war/src/lib.rs
@@ -0,0 +1,2 @@
+pub mod light_list;
+pub mod fog;
---
+++ lt_fog_of_war/src/light_list.rs
@@ -0,0 +1,32 @@
+use lonely_tribes_components::tile_transform::TileTransform;
+use lonely_tribes_components::point_light::PointLight;
+use std::ops::{Deref, DerefMut};
+
+pub type LightListVec = (TileTransform, PointLight);
+
+#[derive(Clone, Debug, Default)]
+pub struct LightList {
+	pub list: Vec<LightListVec>,
+}
+impl Deref for LightList {
+	type Target = Vec<LightListVec>;
+
+	fn deref(&self) -> &Self::Target {
+		&self.list
+	}
+}
+impl DerefMut for LightList {
+	fn deref_mut(&mut self) -> &mut Self::Target {
+		&mut self.list
+	}
+}
+impl LightList {
+	pub fn set(&mut self, list: Vec<LightListVec>) {
+		self.list = list;
+	}
+	pub fn get(&self) -> &[LightListVec] {
+		&self.list
+	}
+}
+
+
